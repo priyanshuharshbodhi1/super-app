bc50b5a79dde272dc221e9e8cb0dbe9e28f0ee6b
priyanshuharshbodhi1 <priyanshuqpwp@gmail.com>
2023-07-22 19:19:37 +0530

    added registration page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -8,35 +8,57 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
   const [shareData, setShareData] = useState(false);
+  const [error, setError] = useState(false);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
+  // const handleNameChange = (event) => {
+  //   setName(event.target.value);
+  // };
+
+  // const handleUsernameChange = (event) => {
+  //   setUsername(event.target.value);
+  // };
+
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // };
+
+  // const handleMobileChange = (event) => {
+  //   setMobile(event.target.value);
+  // };
+
+  // const handleShareDataChange = () => {
+  //   setShareData(!shareData);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      name.length === 0 ||
+      username.length === 0 ||
+      email.length === 0 ||
+      mobile.length === 0
+    ) {
+      setError(true);
+    } else {
+      console.log("Form Submitted");
+      const formData = {
+        name: name,
+        username: username,
+        email: email,
+        mobile: mobile,
+        shareData: shareData,
+      };
+
+      localStorage.setItem("formData", JSON.stringify(formData));
+    }
   };
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleMobileChange = (event) => {
-    setMobile(event.target.value);
-  };
-
-  const handleShareDataChange = () => {
-    setShareData(!shareData);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
   return (
     <>
       <div className="login-page">
         <div className="image-container">
           <img src={LoginImage} alt="" />
+          <p>Discover new things on Superapp</p>
         </div>
         <div className="form-container">
           <form onSubmit={handleSubmit}>
@@ -44,38 +66,65 @@ function LoginPage() {
             <h4>create your account now</h4>
             <input
               type="text"
-              placeholder="Full Name"
+              placeholder="Name"
               value={name}
-              onChange={handleNameChange}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
+            {error && name.length <= 0 ? (
+              <label className="empty-lable">Name can't be empty.</label>
+            ) : (
+              ""
+            )}
             <input
               type="text"
               placeholder="Username"
               value={username}
-              onChange={handleUsernameChange}
+              onChange={(e) => setUsername(e.target.value)}
             />
+            {error && username.length <= 0 ? (
+              <label className="empty-lable">Username can't be empty.</label>
+            ) : (
+              ""
+            )}
             <input
               type="email"
               placeholder="Email"
               value={email}
-              onChange={handleEmailChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            {error && email.length <= 0 ? (
+              <label className="empty-lable">Email can't be empty.</label>
+            ) : (
+              ""
+            )}
             <input
               type="tel"
               placeholder="Mobile"
+              maxLength={10}
               value={mobile}
-              onChange={handleMobileChange}
+              pattern="[0-9]*"
+              onInput={(e) => {
+                e.target.value = e.target.value.replace(/[^0-9]/g, "");
+              }}
+              onChange={(e) => setMobile(e.target.value)}
             />
+            {error && mobile.length <= 0 ? (
+              <label className="empty-lable">Mobile can't be empty.</label>
+            ) : (
+              ""
+            )}
             <div className="checkbox-container">
               <input
                 type="checkbox"
                 checked={shareData}
-                onChange={handleShareDataChange}
+                onChange={(e) => setShareData(e.target.value)}
               />
               <label htmlFor="shareData">
                 Share my registration data with Super App
               </label>
             </div>
+            {/* <label className="empty-lable">Please tick the Checkbox</label> */}
             <button type="submit">Sign Up</button>
             <div className="terms-privacy-container">
               <p>
