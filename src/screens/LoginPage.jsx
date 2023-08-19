202d29a4237c5e821c490c6a0e868dc1b519e800
priyanshu <priyanshuqpwp@gmail.com>
2023-08-19 19:37:57 +0530

    fixed empty field red alert

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -9,20 +9,28 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
   const [shareData, setShareData] = useState(false);
-  const [error, setError] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [mobileError, setMobileError] = useState(false);
+  const [shareDataError, setShareDataError] = useState(false);
 
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
-    
     e.preventDefault();
     if (
       name.length === 0 ||
       username.length === 0 ||
       email.length === 0 ||
       mobile.length === 0 ||
-      shareData === false
+      !shareData
     ) {
-      setError(true);
+      setNameError(name.length === 0);
+      setUsernameError(username.length === 0);
+      setEmailError(email.length === 0);
+      setMobileError(mobile.length === 0);
+      setShareDataError(!shareData);
     } else {
       console.log("Form Submitted");
       const formData = {
@@ -32,15 +40,10 @@ function LoginPage() {
         mobile: mobile,
         shareData: shareData,
       };
-      
       localStorage.setItem("formData", JSON.stringify(formData));
-    };
-
-    navigate('/selectcategory'); 
+      navigate('/selectcategory');
+    }
   };
-
-  
-
 
   return (
     <>
@@ -57,36 +60,39 @@ function LoginPage() {
               type="text"
               placeholder="Name"
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => {
+                setName(e.target.value);
+                setNameError(false);
+              }}
+              className={nameError ? "error-field" : ""}
               required
             />
-            {error && name.length <= 0 ? (
-              <label className="empty-lable">Name can't be empty.</label>
-            ) : (
-              ""
-            )}
+            {nameError ? <label className="empty-label">Name can't be empty.</label> : ""}
+            
             <input
               type="text"
               placeholder="Username"
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                setUsernameError(false);
+              }}
+              className={usernameError ? "error-field" : ""}
             />
-            {error && username.length <= 0 ? (
-              <label className="empty-lable">Username can't be empty.</label>
-            ) : (
-              ""
-            )}
+            {usernameError ? <label className="empty-label">Username can't be empty.</label> : ""}
+            
             <input
               type="email"
               placeholder="Email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setEmailError(false);
+              }}
+              className={emailError ? "error-field" : ""}
             />
-            {error && email.length <= 0 ? (
-              <label className="empty-lable">Email can't be empty.</label>
-            ) : (
-              ""
-            )}
+            {emailError ? <label className="empty-label">Email can't be empty.</label> : ""}
+            
             <input
               type="tel"
               placeholder="Mobile"
@@ -96,25 +102,30 @@ function LoginPage() {
               onInput={(e) => {
                 e.target.value = e.target.value.replace(/[^0-9]/g, "");
               }}
-              onChange={(e) => setMobile(e.target.value)}
+              onChange={(e) => {
+                setMobile(e.target.value);
+                setMobileError(false);
+              }}
+              className={mobileError ? "error-field" : ""}
             />
-            {error && mobile.length <= 0 ? (
-              <label className="empty-lable">Mobile can't be empty.</label>
-            ) : (
-              ""
-            )}
+            {mobileError ? <label className="empty-label">Mobile can't be empty.</label> : ""}
+            
             <div className="checkbox-container">
               <input
                 type="checkbox"
                 checked={shareData}
-                onChange={(e) => setShareData(e.target.checked)}
+                onChange={(e) => {
+                  setShareData(e.target.checked);
+                  setShareDataError(false);
+                }}
               />
               <label htmlFor="shareData">
                 Share my registration data with Super App
               </label>
             </div>
-            {/* <label className="empty-lable">Please tick the Checkbox</label> */}
-            <button type="submit" >Sign Up</button>
+            
+            <button type="submit">Sign Up</button>
+            
             <div className="terms-privacy-container">
               <p>
                 By signing up, you agree to our{" "}
